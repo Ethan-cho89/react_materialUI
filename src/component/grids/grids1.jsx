700065bc1c8c1bbebb54f2d415922c2278c26998
ethan <bjaguar89@gmail.com>
2021-05-13 17:51:32 +0900

    grid

--- src/component/grids/grids1.jsx
+++ src/component/grids/grids1.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import Grid from '@material-ui/core/grid';
 import Box from '@material-ui/core/box';
-
+import Hidden from '@material-ui/core/hidden';
 const Grids1 = () => {
   const message = 'hello';
 
   return (
     <Grid container justify="center" spacing={1}>
-      <Grid item xs={5} md={4} lg={12}>
-        <Box bgcolor="info.main" p={3}>
-          {message}1
-        </Box>
-      </Grid>
-      <Grid item xs={5} md={4} lg={12}>
-        <Box bgcolor="info.main" p={3}>
-          {message}2
-        </Box>
-      </Grid>
-      <Grid item xs={2} md={4} lg={12}>
-        <Box bgcolor="info.main" p={3}>
-          {message}3
-        </Box>
-      </Grid>
+      <Hidden only="xl">
+        <Grid item xs={5} md={4} lg={12}>
+          <Box bgcolor="info.main" p={3}>
+            {message}1
+          </Box>
+        </Grid>
+      </Hidden>
+      <Hidden xsDown>
+        <Grid item xs={5} md={4} lg={12}>
+          <Box bgcolor="info.main" p={3}>
+            {message}2
+          </Box>
+        </Grid>
+      </Hidden>
+      <Hidden lgUp>
+        <Grid item xs={2} md={4} lg={12}>
+          <Box bgcolor="info.main" p={3}>
+            {message}3
+          </Box>
+        </Grid>
+      </Hidden>
       <Grid container item>
         <Grid item xs={12}>
           <Box bgcolor="info.main" p={3}>
